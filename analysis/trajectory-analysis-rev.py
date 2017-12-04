e957590a3a443226f8dc82784a33b3f29f3c9e46
Raayan Pillai <raayanp01@gmail.com>
2017-12-04 15:40:28 -0500

    Results for using Indeed and LinkedIn for trajectory analysis

--- analysis/trajectory-analysis-rev.py
+++ analysis/trajectory-analysis-rev.py
@@ -7,7 +7,15 @@ from sklearn import tree
 
 users = []
 test_users = []
+
+#Load LinkedIn Data
 with open('../data/users/user_traj_form.csv', 'rb') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in csvreader:
+        users.append([int(x) for x in row])
+
+#Load Indeed Data
+with open('../data/users/indeed/ser_form.csv', 'rb') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in csvreader:
         users.append([int(x) for x in row])
@@ -26,110 +34,111 @@ with open('../data/users/best_schools.csv', 'rb') as csvfile:
     for row in csvreader:
         top_schools.append(row[1])
 
-test_floor = 300
-test_users = users[test_floor:]
-users = users[:test_floor]
-print(len(users), len(test_users))
-# Main binning
-#company_bins = [0, 50, 200, 500, len(top_companies), len(top_companies)+1, len(top_companies)+2]
-#company_bin_labels = ['T1', 'T2', 'T3', 'T4', 'T5', 'NONE']
-#school_bins = [0, 50, 200, 500, len(top_schools), len(top_schools)+1, len(top_schools)+2]
-#school_bin_labels = ['T1', 'T2', 'T3', 'T4', 'T5', 'NONE']
+base_users = users
+for floor in range(300, len(users)-10, 10):
+    test_floor = floor 
+    test_users = base_users[test_floor:]
+    users = base_users[:test_floor]
+    # Main binning
+    #company_bins = [0, 50, 200, 500, len(top_companies), len(top_companies)+1, len(top_companies)+2]
+    #company_bin_labels = ['T1', 'T2', 'T3', 'T4', 'T5', 'NONE']
+    #school_bins = [0, 50, 200, 500, len(top_schools), len(top_schools)+1, len(top_schools)+2]
+    #school_bin_labels = ['T1', 'T2', 'T3', 'T4', 'T5', 'NONE']
 
-# Alternate Binning
-company_bins = [0, 50, 200, 500, len(top_companies), len(top_companies)+1, len(top_companies)+2]
-company_bin_labels = ['T1', 'T2', 'T3', 'T4', 'T5', 'NONE']
-school_bins = [0, 50, 200, 500, len(top_schools), len(top_schools)+1, len(top_schools)+2]
-school_bin_labels = ['T1', 'T2', 'T3', 'T4', 'T5', 'NONE']
+    # Alternate Binning
+    company_bins = [0, 50, 200, 500, len(top_companies), len(top_companies)+1, len(top_companies)+3]
+    company_bin_labels = ['T1', 'T2', 'T3', 'T4', 'T5', 'NONE']
+    school_bins = [0, 50, 200, 500, len(top_schools), len(top_schools)+1, len(top_schools)+3]
+    school_bin_labels = ['T1', 'T2', 'T3', 'T4', 'T5', 'NONE']
 
-def formatCompany(company):
-    if company == 'N/A':
-        return len(top_companies)+2
-    best = difflib.get_close_matches(company, top_companies)
-    if len(best) == 0:
-        return len(top_companies)+1
-    else:
-        return top_companies.index(best[0])
+    def formatCompany(company):
+        if company == 'N/A':
+            return len(top_companies)+2
+        best = difflib.get_close_matches(company, top_companies)
+        if len(best) == 0:
+            return len(top_companies)+1
+        else:
+            return top_companies.index(best[0])
 
-def formatSchool(school):
-    if school == 'N/A':
-        return len(top_schools)+2
-    best = difflib.get_close_matches(school, top_schools)
-    if len(best) == 0:
-        return len(top_schools)+1
-    else:
-        return top_schools.index(best[0])
+    def formatSchool(school):
+        if school == 'N/A':
+            return len(top_schools)+2
+        best = difflib.get_close_matches(school, top_schools)
+        if len(best) == 0:
+            return len(top_schools)+1
+        else:
+            return top_schools.index(best[0])
 
-def formatUser(user):
-    return [formatSchool(user[0])] + [formatCompany(x) for x in user[1:]]
+    def formatUser(user):
+        return [formatSchool(user[0])] + [formatCompany(x) for x in user[1:]]
 
-schools_lab = [x[0] for x in users]
-companies1_lab= [x[1] for x in users]
-companies2_lab= [x[2] for x in users]
-companies3_lab= [x[3] for x in users]
-
-
-#users = users[:10]
-schools = [int(x[0]) for x in users]
-companies1 = [int(x[1]) for x in users]
-companies2 = [int(x[2]) for x in users]
-companies3 = [int(x[3]) for x in users]
-
-schools_bin = pd.cut(schools, school_bins, labels=school_bin_labels)
-companies1_bin = pd.cut(companies1, company_bins, labels=company_bin_labels)
-companies2_bin = pd.cut(companies2, company_bins, labels=company_bin_labels)
-companies3_bin = pd.cut(companies3, company_bins, labels=company_bin_labels)
-
-def binUser(user):
-    return [pd.cut([user[0]], school_bins, labels=school_bin_labels)] + [pd.cut([x], company_bins, labels=company_bin_labels) for x in user[1:]]
-
-#Preprocess data
-school_pre = preprocessing.LabelEncoder()
-school_pre.fit(school_bin_labels)
-company_pre = preprocessing.LabelEncoder()
-company_pre.fit(company_bin_labels)
-
-schools_bin = school_pre.transform(schools_bin)
-companies1_bin = company_pre.transform(companies1_bin)
-companies2_bin = company_pre.transform(companies2_bin)
-companies3_bin = company_pre.transform(companies3_bin)
+    schools_lab = [x[0] for x in users]
+    companies1_lab= [x[1] for x in users]
+    companies2_lab= [x[2] for x in users]
+    companies3_lab= [x[3] for x in users]
 
 
-#pre process single user
-def ppUser(user):
-     return [school_pre.transform(user[0])] + [company_pre.transform(x) for x in user[1:]]
-    
+    #users = users[:10]
+    schools = [int(x[0]) for x in users]
+    companies1 = [int(x[1]) for x in users]
+    companies2 = [int(x[2]) for x in users]
+    companies3 = [int(x[3]) for x in users]
 
-def makeTestCase(user):
-    #formatted_user = formatUser(user)
-    formatted_user = user
-    binned_user = binUser(formatted_user)
-    pp_user = ppUser(binned_user)
-    pp_user = [x[0] for x in pp_user]
-    return [pp_user]
+    schools_bin = pd.cut(schools, school_bins, labels=school_bin_labels)
+    companies1_bin = pd.cut(companies1, company_bins, labels=company_bin_labels)
+    companies2_bin = pd.cut(companies2, company_bins, labels=company_bin_labels)
+    companies3_bin = pd.cut(companies3, company_bins, labels=company_bin_labels)
 
-data = pd.DataFrame(data={'school': schools_bin,'company1': companies1_bin,'company2': companies3_bin,'company3': companies3_bin})
+    def binUser(user):
+        return [pd.cut([user[0]], school_bins, labels=school_bin_labels)] + [pd.cut([x], company_bins, labels=company_bin_labels) for x in user[1:]]
 
-# create classifier
-dtc = tree.DecisionTreeClassifier()
-dtc = dtc.fit(data[['school','company1','company2']], data['company3'])
+    #Preprocess data
+    school_pre = preprocessing.LabelEncoder()
+    school_pre.fit(school_bin_labels)
+    company_pre = preprocessing.LabelEncoder()
+    company_pre.fit(company_bin_labels)
 
-# Easy Predit Method
-def predict(user):
-    test = makeTestCase(user[:3])
-    return dtc.predict(test)
+    schools_bin = school_pre.transform(schools_bin)
+    companies1_bin = company_pre.transform(companies1_bin)
+    companies2_bin = company_pre.transform(companies2_bin)
+    companies3_bin = company_pre.transform(companies3_bin)
 
-# test a data set
-# test = ['Binghamton University', 'Arity', 'Cisco']
-# test_pre = makeTestCase(test)
-# test_out = dtc.predict(test_pre)
-# print(test_pre, test_out)
-corrects = 0
-for test in test_users:
-    test_case = makeTestCase(test)
-    test_result = predict(test)
-    correct = test_case[0][3] == test_result[0]
-    if correct:
-        corrects += 1
 
-print("Result: ", corrects*1.0/len(test_users))
+    #pre process single user
+    def ppUser(user):
+        return [school_pre.transform(user[0])] + [company_pre.transform(x) for x in user[1:]]
+        
+
+    def makeTestCase(user):
+        #formatted_user = formatUser(user)
+        formatted_user = user
+        binned_user = binUser(formatted_user)
+        pp_user = ppUser(binned_user)
+        pp_user = [x[0] for x in pp_user]
+        return [pp_user]
+
+    data = pd.DataFrame(data={'school': schools_bin,'company1': companies1_bin,'company2': companies3_bin,'company3': companies3_bin})
+
+    # create classifier
+    dtc = tree.DecisionTreeClassifier()
+    dtc = dtc.fit(data[['school','company1','company2']], data['company3'])
+
+    # Easy Predit Method
+    def predict(user):
+        test = makeTestCase(user[:3])
+        return dtc.predict(test)
+
+    # test a data set
+    # test = ['Binghamton University', 'Arity', 'Cisco']
+    # test_pre = makeTestCase(test)
+    # test_out = dtc.predict(test_pre)
+    # print(test_pre, test_out)
+    corrects = 0
+    for test in test_users:
+        test_case = makeTestCase(test)
+        test_result = predict(test)
+        correct = test_case[0][3] == test_result[0]
+        if correct:
+            corrects += 1
+
+    print(len(users), len(test_users), corrects*1.0/len(test_users))
